7567917491b97beb1d508eab5c3ab52ec74e29b5
n.zbiranik <n.zbiranik@cdek.ru>
2025-08-27 12:12:32 +0700

    add strings

---
+++ string/builder_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+var x, y, z int = 1, 20, 309
+
+func UseFmt() string {
+	return fmt.Sprintf("%d_%d_%d_group", x, y, z)
+}
+
+func UseBuilder() string {
+	var b strings.Builder
+	b.WriteString(strconv.Itoa(x))
+	b.WriteString("_")
+	b.WriteString(strconv.Itoa(y))
+	b.WriteString("_")
+	b.WriteString(strconv.Itoa(z))
+	b.WriteString("_group")
+	return b.String()
+}
+func BenchmarkFmt(b *testing.B) {
+	for b.Loop() {
+		UseFmt()
+	}
+}
+
+func BenchmarkBuilder(b *testing.B) {
+	for b.Loop() {
+		UseBuilder()
+	}
+}
